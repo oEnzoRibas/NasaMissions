58612ff162a193a994a846c3c80b621dfeb6c3e6
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-06-07 19:29:59 -0300

    .

--- database/Queries/select tripulante by char.sql
+++ database/Queries/select tripulante by char.sql
@@ -1,6 +1,7 @@
-USE NASA_Missions
-SELECT * FROM Tripulantes T
-JOIN NAVES N ON t.id_nave = n.id_nave
-WHERE T.nome_tripulante LIKE 'e%'
+-- Switch to the NASA_Missions database (in psql, use \c NASA_Missions)
+-- \c NASA_Missions
 
-delete from Tripulantes where id_tripulante
+SELECT * FROM Tripulantes T
+JOIN Naves N ON T.id_nave = N.id_nave
+WHERE T.nome_tripulante ILIKE 'e%';
+
